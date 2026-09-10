56eb195e6b889898558a1d3fc89ab59d9d8b65a0
sharvarimodi3 <sharvariwork03@gmail.com>
2026-09-10 16:52:23 +0530

    6 programs done

--- program.py
+++ program.py
@@ -8,12 +8,26 @@
 #print("Years = ", age_in_days, "days.")
 
 #3.
-minutes = int(input("Enter minutes: "))
-hours = minutes / 60
-remaining_minutes = minutes % 60
-print(f'{minutes} is {int(hours)} Hours and {remaining_minutes} Minutes')
+#minutes = int(input("Enter minutes: "))
+#hours = minutes / 60
+#remaining_minutes = minutes % 60
+#print(f'{minutes} is {int(hours)} Hours and {remaining_minutes} Minutes')
 
 #4.
-integer = int(input("Enter integers: "))
-last_digit = integer % 10
-print("Last digit of the number is:", last_digit)
+#integer = int(input("Enter integers: "))
+#last_digit = integer % 10
+#print("Last digit of the number is:", last_digit)
+
+#5.
+role = input("Enter your role: ")
+age = int(input("Enter your age: "))
+print("Eligible :", role == "student" and age >= 21)
+
+#6.
+a = int(input("Enter A number: "))
+b = int(input("Enter B number: "))
+print("Before swap : a=",a,"b =",b)
+a = a + b
+b = a - b
+a = a - b
+print("After Swap : a=",a,"b =",b)
