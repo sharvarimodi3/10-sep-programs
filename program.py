3d5f3f8fb56326a4044ab83a357779e2e53f0757
sharvarimodi3 <sharvariwork03@gmail.com>
2026-09-10 15:15:26 +0530

    3 program

---
+++ program.py
@@ -0,0 +1,14 @@
+#number = int(input("Enter a number:"))
+#is_odd = number % 2 != 0
+#print("Number is odd:", is_odd)
+
+#2.
+#age = int(input("Enter your age:"))
+#age_in_days = age * 365
+#print("Years = ", age_in_days, "days.")
+
+#3.
+minutes = int(input("Enter minutes: "))
+hours = minutes / 60
+remaining_minutes = minutes % 60
+print(f'Hours: {int(hours)} Remaining Minutes: {remaining_minutes}')
